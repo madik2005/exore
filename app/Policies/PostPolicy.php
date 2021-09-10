7b98c34d9b6aae26139b4737249a57c184094ffe
madik2005 <madik2005@tut.by>
2021-09-10 13:46:17 +0300

    delete PostPolicy method

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -22,32 +22,9 @@ class PostPolicy
         if ($user->role == 'employee'){
             return $user->id == $post->user_id;
         }
-        //dd($post);
         return true;
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function updatePost(User $user, Post $post)
-    {
-        //
-    }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function deletePost(User $user, Post $post)
-    {
-        //
-    }
 
 }
